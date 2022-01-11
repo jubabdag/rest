d9be0527eae1ac7d8d99a92941d5545e7a872dd4
jubabdag <yoshikawa.juvenal@gmail.com>
2022-01-11 11:07:58 -0300

    Adição da configuração de crypto como variáeisde ambiente

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
+import config from 'config';
 import { Pool } from 'pg';
 
-const connectionString = '';
+const connectionString = config.get<string>('database.uri');
 
 const db = new Pool({ connectionString });
 
